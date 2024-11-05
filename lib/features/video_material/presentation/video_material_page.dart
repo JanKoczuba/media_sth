74fd0f3178932cd372a1289bf0aa2fe76da14c9b
Jan Koczuba <schizobi@gmail.com>
2024-11-05 16:50:02 +0100

    added video

--- lib/features/video_material/presentation/video_material_page.dart
+++ lib/features/video_material/presentation/video_material_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:media_sth/core/app/video_assets.dart';
+import 'package:media_sth/features/video_material/presentation/widgets/video_content.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoMaterialPage extends StatefulWidget {
   const VideoMaterialPage({super.key});
@@ -8,8 +11,48 @@ class VideoMaterialPage extends StatefulWidget {
 }
 
 class _VideoMaterialPageState extends State<VideoMaterialPage> {
+  late final VideoPlayerController videoPlayerController;
+  late final screenHeight = MediaQuery.sizeOf(context).height;
+
+  @override
+  void initState() {
+    super.initState();
+    videoPlayerController = VideoPlayerController.asset(VideoAssets.bee);
+    _videoInit();
+  }
+
+  Future _videoInit() async {
+    try {
+      await videoPlayerController.initialize();
+      videoPlayerController.play();
+    } catch (e) {
+      //TODO
+    }
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            right: 0,
+            left: 0,
+            top: 0,
+            bottom: (screenHeight / 3) * 2,
+            child: VideoContent(
+              controller: videoPlayerController,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
